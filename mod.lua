90bf8173de85886a9a1d2a7d3053a198e4585056
Florian Thiel <flth@flth.de>
2023-07-09 14:40:04 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "waggon", },
+		minGameVersion = 0,
+		dependencies = { },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+	
+	end
+	}
+end
